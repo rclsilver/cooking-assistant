9c2b0ee6c8a87b2d82f3151bae5813e71ed7a578
Thomas Bétrancourt <thomas.betrancourt@ovhcloud.com>
2020-11-29 16:26:07 +0000

    [frontend] Initialize recipes list

--- frontend/src/app/recipes/recipe-list/recipe-list.component.ts
+++ frontend/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,13 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AuthService } from 'src/app/auth/auth.service';
+import { ApiService } from 'src/app/shared/api.service';
+import { ConfirmDialogComponent } from 'src/app/shared/confirm-dialog/confirm-dialog.component';
+import { SuccessDialogComponent } from 'src/app/shared/success-dialog/success-dialog.component';
+import { Recipe } from '../types';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +15,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+  isAuthenticated$: Observable<boolean> = this.auth.isAuthenticated$;
+  isAdministrator$: Observable<boolean> = this.auth.isAdministrator$;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private api: ApiService,
+    private breakpointObserver: BreakpointObserver,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
+    this.refresh();
   }
 
+  refresh(): void {
+    this.api.getRecipes()
+      .then((recipes: Recipe[]) => this.recipes = recipes)
+      .catch(this.api.handleServerError('Error while fetching recipes list'));
+  }
+
+  deleteRecipe(recipe: Recipe): void {
+    this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'Recipe deletion',
+        message: 'Are you sure to delete this recipe?',
+        confirmText: 'Yes, drop it!'
+      }
+    }).afterClosed().subscribe((confirm: boolean) => {
+      if (confirm) {
+        this.api.deleteRecipe(recipe)
+          .then(() => {
+            this.recipes = this.recipes.filter((item) => item.id != recipe.id);
+            this.dialog.open(SuccessDialogComponent, {
+              data: {
+                title: 'Recipe deletion',
+                message: 'Recipe has been removed'
+              }
+            });
+          })
+          .catch(this.api.handleServerError('Error while deleting recipe'));
+      }
+    });
+  }
 }
--- frontend/src/app/recipes/recipes.module.ts
+++ frontend/src/app/recipes/recipes.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @NgModule({
@@ -7,7 +10,10 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
     RecipeListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class RecipesModule { }
